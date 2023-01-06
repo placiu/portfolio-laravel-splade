93376e9d967a57d6f101a55e1cc57d0a09288de3
placiu <placiu@gmail.com>
2023-01-06 13:21:50 +0100

    narrow spaces between navigation items

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -12,7 +12,7 @@
                     </div>
 
                     <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <div class="hidden space-x-4 sm:-my-px sm:ml-10 sm:flex">
                         <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
                             {{ __('Home') }}
                         </x-nav-link>
