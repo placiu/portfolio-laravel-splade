7bd71971dd1c4237aef8acabbb53b1ef7dc1221a
placiu <placiu@gmail.com>
2023-01-06 13:19:19 +0100

    prepare footer with social icons

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,8 @@
-<footer class="w-full max-w-7xl mx-auto overflow-hidden">
-    <span class="text-secondary">
-        
-    </span>
+<footer class="relative w-full max-w-7xl mx-auto py-4">
+    <span class="text-xs text-secondary font-sans">&copy; 2023 Paweł Płatek</span>
+    <div class="absolute flex flex-col justify-center items-center space-y-4 bottom-0 right-2">
+        <x-coolicon-linkedin class="h-10 w-10 text-secondary" stroke="currentColor" fill="none" viewBox="0 0 24 24" />
+        <x-coolicon-github class="h-10 w-10 text-secondary" stroke="currentColor" fill="none" viewBox="0 0 24 24" />
+        <div class="flex w-[1px] sm:h-24 md:h-48 lg:h-96 bg-secondary"></div>
+    </div>
 </footer>
