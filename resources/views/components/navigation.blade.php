6f1e292e63f3f4b4efc51e5df8cab700a57ca336
placiu <placiu@gmail.com>
2023-01-06 12:07:39 +0100

    replace navigation svgs with icons

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -38,10 +38,8 @@
                 <!-- Hamburger -->
                 <div class="-mr-2 flex items-center sm:hidden">
                     <button @click="data.open = ! data.open" class="inline-flex items-center justify-center p-2 rounded-md text-primary hover:text-primary hover:bg-darker focus:outline-none focus:bg-darker focus:primary transition">
-                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                            <path v-bind:class="{'hidden': data.open, 'inline-flex': ! data.open }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                            <path v-bind:class="{'hidden': ! data.open, 'inline-flex': data.open }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
+                        <x-coolicon-hamburger v-bind:class="{'hidden': data.open, 'inline-flex': ! data.open }" class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24" />
+                        <x-coolicon-close-big v-bind:class="{'hidden': ! data.open, 'inline-flex': data.open }" class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24" />
                     </button>
                 </div>
             </div>
