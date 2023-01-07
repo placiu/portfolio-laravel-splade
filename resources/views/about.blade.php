e969bada0076e999b91a63a1e4d4d52ae049503f
placiu <placiu@gmail.com>
2023-01-07 13:22:35 +0100

    prepare about me page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,35 @@
 <x-layout>
     <x-content>
-        
+        <div class="flex flex-col w-full h-full justify-center space-y-14">
+            <div class="px-20 font-sans font-extrabold">
+                <span class="text-4xl text-primary">
+                    About
+                </span>
+                <span class="text-4xl text-accent">
+                    Me. 
+                </span>
+            </div>
+            <div class="flex px-20 font-sans">
+                <div class="flex flex-col space-y-6 text-secondary w-3/4 leading-8 italic">
+                    <p>My name is Paweł and my passion is web development. I love working and discovering new tools and technologies. I’m 38, living near Wrocław, Poland.</p>
+                    <p>My interest in webdev started back in primary school when I published my first website. It was an URL aggregator for recommended websites. Google wasn’t so popular back then ;)</p>
+                    <p>Over the years I treated this as my hobby and a side gig, helping small, local business move to internet. It was 2016 when I decided I want to make it my full time job. I was lucky enough to get a chance at <a href="https://codingmice.com/" class="text-accent" target="_blank">Coding Mice</a> where I’m working for over 4 years now. </p>
+                </div>
+            </div>
+            <div class="flex space-x-4 px-20 font-sans">
+                <a href="{{ route('contact') }}" class="flex space-x-2 px-6 py-4 bg-dark border border-accent text-accent rounded">
+                    <x-coolicon-mail class="h-5 w-5 text-accent" stroke="currentColor" fill="none" viewBox="0 0 24 24" />
+                    <span class="font-bold uppercase text-sm">
+                        Contact
+                    </span>
+                </a>
+                <a href="" class="flex space-x-2 px-6 py-4 bg-dark border border-accent text-accent rounded">
+                    <x-coolicon-download class="h-5 w-5 text-accent" stroke="currentColor" fill="none" viewBox="0 0 24 24" />
+                    <span class="font-bold uppercase text-sm">
+                        Resume
+                    </span>
+                </a>
+            </div>
+        </div>
     </x-content>
 </x-layout>
