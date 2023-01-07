55dbbad69480e51e8f58ec04d6cde294913c64ec
placiu <placiu@gmail.com>
2023-01-07 13:25:55 +0100

    prepare sandbox page

--- resources/views/sandbox.blade.php
+++ resources/views/sandbox.blade.php
@@ -1,5 +1,26 @@
 <x-layout>
     <x-content>
-        
+        <div class="flex flex-col w-full h-full justify-center space-y-14">
+            <div class="px-20 font-sans font-extrabold">
+                <span class="text-4xl text-primary">
+                    My
+                </span>
+                <span class="text-4xl text-accent">
+                    Sandbox. 
+                </span>
+            </div>
+            <div class="px-20 grid grid-cols-5 gap-7">
+                <div class="flex items-center justify-center w-[200px] h-[200px] bg-accent"></div>
+                <div class="flex items-center justify-center w-[200px] h-[200px] bg-accent"></div>
+                <div class="flex items-center justify-center w-[200px] h-[200px] bg-accent"></div>
+                <div class="flex items-center justify-center w-[200px] h-[200px] bg-accent"></div>
+                <div class="flex items-center justify-center w-[200px] h-[200px] bg-accent"></div>
+                <div class="flex items-center justify-center w-[200px] h-[200px] bg-accent"></div>
+                <div class="flex items-center justify-center w-[200px] h-[200px] bg-accent"></div>
+                <div class="flex items-center justify-center w-[200px] h-[200px] bg-accent"></div>
+                <div class="flex items-center justify-center w-[200px] h-[200px] bg-accent"></div>
+                <div class="flex items-center justify-center w-[200px] h-[200px] bg-accent"></div>
+            </div>
+        </div>
     </x-content>
 </x-layout>
