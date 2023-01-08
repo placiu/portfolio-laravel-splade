eba722b418e62736d809bcd6be70465b0eb7dc77
placiu <placiu@gmail.com>
2023-01-08 21:53:52 +0100

    add more dummy projects in sandbox

--- resources/views/sandbox.blade.php
+++ resources/views/sandbox.blade.php
@@ -2,7 +2,15 @@
     <x-content>
         <x-header text="My Sandbox"/>
         <div class="flex flex-col md:flex-row flex-wrap gap-4 items-center">
-            <x-project-thumbnail></x-project-thumbnail>
+            <x-project-thumbnail>
+                <span class="text-xl text-dark font-extrabold">soon</span>
+            </x-project-thumbnail>
+            <x-project-thumbnail>
+                <span class="text-xl text-dark font-extrabold">soon</span>
+            </x-project-thumbnail>
+            <x-project-thumbnail>
+                <span class="text-xl text-dark font-extrabold">soon</span>
+            </x-project-thumbnail>
         </div>
     </x-content>
 </x-layout>
