e8074c8f5e578ea0dd32209c279649e1d4a32c00
placiu <placiu@gmail.com>
2023-01-08 10:46:55 +0100

    rewrite social icons to be in the page center

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,12 @@
 <footer class="container relative mx-auto mt-auto py-2 px-2 md:px-4 lg:px-6">
     <span class="text-xs text-secondary font-sans">&copy; 2023 Paweł Płatek</span>
     <div class="absolute flex flex-col justify-center items-center space-y-4 bottom-0 right-4 md:right-8 lg:right-12">
-        <x-coolicon-linkedin class="h-10 w-10 text-secondary" stroke="currentColor" fill="none" viewBox="0 0 24 24" />
-        <x-coolicon-github class="h-10 w-10 text-secondary" stroke="currentColor" fill="none" viewBox="0 0 24 24" />
-        <div class="flex w-[1px] h-24 md:h-48 lg:h-96 bg-secondary"></div>
+        <a href="https://www.linkedin.com/in/pawe%C5%82-p%C5%82atek/" target="_blank">
+            <x-coolicon-linkedin class="h-10 w-10 text-secondary" stroke="currentColor" fill="none" viewBox="0 0 24 24" />
+        </a>
+        <a href="https://github.com/placiu" target="_blank">
+            <x-coolicon-github class="h-10 w-10 text-secondary" stroke="currentColor" fill="none" viewBox="0 0 24 24" />
+        </a>
+        <div class="flex w-[1px] page-center bg-secondary"></div>
     </div>
 </footer>
